59a7ff1cd37bb3a0855b0e91010e48af3ba29fe3
SebastianS-HTL <hexatex.sesh@gmail.com>
2025-01-17 12:00:41 +0100

    add viewport aproach

--- pause menu/scripts/fov_slider.gd
+++ pause menu/scripts/fov_slider.gd
@@ -5,5 +5,7 @@ func _process(delta):
 	var output = get_parent().get_child(6)
 	var player = get_parent().get_parent().get_parent().get_parent().get_child(0)
 	var camera = player.get_child(1)
+	var camera2 = player.get_child(2).get_child(0).get_child(0)
 	output.set_text(str(value))
 	camera.baseFOV = value
+	#camera2.fov = value
