bdbbb6110b38343b750ddf9680df6a1012a84af5
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-02-22 18:30:39 +0100

    add removing functionality of the crosshair

--- pause menu/scripts/crosshair_slider.gd
+++ pause menu/scripts/crosshair_slider.gd
@@ -5,5 +5,12 @@ func _process(delta):
 	var output = get_parent().get_child(1)
 	var player = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_child(0)
 	var crosshair = player.get_child(2).get_child(0).get_child(0).get_child(1)
+	var show:CheckButton = $"../../ScaleBox4/show?"
+	
+	if show.is_pressed():
+		crosshair.show()
+	else:
+		crosshair.hide()
+	
 	output.set_text(str(value))
 	crosshair.scale = Vector3(value,value,value)
