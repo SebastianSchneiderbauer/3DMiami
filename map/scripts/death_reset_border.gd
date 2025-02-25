c6f5a86aae367b646b6fd4ca21a70f2412eddc47
SebastianS-HTL <hexatex.sesh@gmail.com>
2025-02-25 10:30:39 +0100

    add to be implemented propertys in the deathzone

--- map/scripts/death_reset_border.gd
+++ map/scripts/death_reset_border.gd
@@ -1,6 +1,8 @@
 extends Area3D
 
 @export var reset_position:Vector3 = Vector3(0,0,0)
+@export var damage:int = 0
+@export var stop_at_1:bool = true
 
 func _on_body_entered(body):
 	body.position = reset_position
