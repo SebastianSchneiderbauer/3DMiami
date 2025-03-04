9d9acf442bf556560b86ef2aec5414f5b29592d2
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-03-04 23:16:23 +0100

    start working on dummy logic

--- dummy/scripts/dummy.gd
+++ dummy/scripts/dummy.gd
@@ -1,5 +1,20 @@
 extends CharacterBody3D
 
+const default_gravity:Vector3 = Vector3(0,-9.8,0)
+var used_gravity: Vector3 = default_gravity
+var gravity_changer:float = 20 #ridiculously high, bc this without deltatime would be crazy
+
+func _physics_process(delta: float) -> void:
+	#increase gravity while falling
+	if velocity.y < 0 and not is_on_wall():
+		used_gravity.y -= gravity_changer*delta
+	else:
+		used_gravity = default_gravity
+	
+	if not is_on_floor():
+		velocity += used_gravity * delta
+	
+	move_and_slide()
 
 func _on_player_push_area_body_entered(body: Node3D) -> void:
 	print("collision detected") #would have to trigger a push
