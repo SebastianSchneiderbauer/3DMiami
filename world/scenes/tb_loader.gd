5f7e54320d1a77457a956425b7e5a7b0da53d9bb
SebastianS-HTL <hexatex.sesh@gmail.com>
2025-03-19 12:08:55 +0100

    TBLoader now also sets up sampling and transparency

--- world/scenes/tb_loader.gd
+++ world/scenes/tb_loader.gd
@@ -4,3 +4,10 @@ func _ready():
 	for child in get_children():
 		if child is OmniLight3D:
 			child.shadow_enabled = true
+	
+	#set everything to be sharp
+	get_node("Default Layer/entity_0_geometry").mesh.surface_get_material(0).set_texture_filter(0)
+	#make barriers invisible
+	get_node("barriers/entity_13_geometry").mesh.surface_get_material(0).set_transparency(1)
+
+	
