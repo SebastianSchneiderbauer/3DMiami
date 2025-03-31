a711d6089bef6941481d1427c29cf77b1d988c75
SebastianS-HTL <hexatex.sesh@gmail.com>
2025-03-31 12:00:04 +0200

    rework outline thickness
    should now always be kinda the same

--- entities/dummy/scripts/dummy.gd
+++ entities/dummy/scripts/dummy.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 
 @onready var player = get_parent().get_node("player")
 
+var outline_width:float = 5
+
 var selected:bool = false
 var highlightMax: int = 5
 
@@ -11,12 +13,14 @@ const default_gravity:Vector3 = Vector3(0,-9.8,0)
 var used_gravity: Vector3 = default_gravity
 var gravity_changer:float = 20 #ridiculously high, bc this without deltatime would be crazy
 
+var loaded:bool = false
+
 func movement(delta:float):
 	#increase gravity while falling
 	if velocity.y < 0 and not is_on_wall():
 		used_gravity.y -= gravity_changer*delta
 	else:
-		used_gravity = default_gravity
+		used_gravity = default_gravity 
 	
 	if not is_on_floor():
 		velocity += used_gravity * delta
@@ -34,6 +38,31 @@ func _physics_process(delta: float) -> void:
 	mesh_instance_3d.show
 	movement(delta)
 	turnAirdashDetectionToPlayer()
+
+func updateOutline(_strength:float):
+	var strength = _strength
+	var underOneMUltiplier = 1
+	
+	if strength < 1:
+		underOneMUltiplier = 20 - 19*strength
+	
+	var shader_mat := $selectHighlight.material_overlay as ShaderMaterial
+	if shader_mat:
+		shader_mat.set_shader_parameter("outline_width", outline_width*strength*underOneMUltiplier)
+		print(outline_width*strength)
+
+func _process(delta):
+	if SaveManager.loaded and not loaded:
+		loaded = true
+		var type:int = SaveManager.get_data("scaler")
+		var strength:float
+		
+		if type == 1:
+			strength = SaveManager.get_data("scalerStrength")/100
+		else:
+			strength = get_fsr_value(SaveManager.get_data("fsrStrength"))/100
+		
+		updateOutline(strength)
 
 func getDist(hitPosition: Vector3):
 	var space_state = get_world_3d().direct_space_state
@@ -54,3 +83,20 @@ func getDist(hitPosition: Vector3):
 	var midPosition := global_position
 	midPosition.y += 1
 	return (hitPosition - midPosition).length()
+
+func get_fsr_value(index:int):
+	match index:
+		1:
+			1.0
+		2:
+			25.0
+		3:
+			50.0
+		4:
+			66.0
+		5:
+			77.0
+		6:
+			83.0
+		7:
+			125.00
