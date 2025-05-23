bb6445143b487485bd20d16dd6fb94bc7148c143
SebastianS-HTL <hexatex.sesh@gmail.com>
2025-05-23 12:28:24 +0200

    try at a implementation of light-mode

--- world-00/scripts/fullbright.gd
+++ world-00/scripts/fullbright.gd
@@ -9,6 +9,18 @@ func hk_glow():
 	glow = not glow
 	
 	if glow: 
+		#toggleLightmap(0) //completely useless piece of shit
 		environment.background_energy_multiplier = 16
 	else:
+		#toggleLightmap(0)
 		environment.background_energy_multiplier = 1
+	
+	
+
+func toggleLightmap(value:int):
+	var tbloader = get_parent().get_child(1)
+	var toggleVariable
+	
+	for child in tbloader.get_children():
+		if "Layer" in child.name and child is Node3D:
+			child.get_child(0).set_gi_mode(value)
